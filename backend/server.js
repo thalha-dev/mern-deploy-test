37211b30482bcf3ac62f0cad2625446fb053a36a
thalha-dev <dev.thalha@gmail.com>
2023-06-06 11:22:29 +0530

    mern deploy test

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
+
+// express app
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 4000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
